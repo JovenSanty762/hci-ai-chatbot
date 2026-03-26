2d2bdc3f34719591bbe50784d60d1b58968ce666
JovenSanty762 <burbanojaviere22@gmail.com>
2026-03-26 16:20:56 -0500

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+from datetime import date, time
 
+class AppointmentCreate(BaseModel):
+    patient_name: str
+    patient_document: str
+    specialty: str
+    appointment_date: date
+    appointment_time: time
+
+class AppointmentResponse(AppointmentCreate):
+    id: int
+    status: str
+
+    class Config:
+        from_attributes = True
