b00d0ab34d1d6416ade531c52a44f4992cbbcef5
JovenSanty762 <burbanojaviere22@gmail.com>
2026-03-26 16:23:32 -0500

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,1 +1,15 @@
-
+export default function Header() {
+  return (
+    <div style={{
+      padding: "15px",
+      backgroundColor: "#0066cc",
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+      borderTopLeftRadius: "10px",
+      borderTopRightRadius: "10px"
+    }}>
+      🏥 Hospital - Asistente Virtual de Citas
+    </div>
+  );
+}
