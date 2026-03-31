5fd80748bd365ad97fdf10bf5b880119de46accb
JovenSanty762 <burbanojaviere22@gmail.com>
2026-03-31 07:44:49 -0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from .config import DATABASE_URL
 from .logger import logger
+from .database import Base
 
 try:
     engine = create_engine(DATABASE_URL)
